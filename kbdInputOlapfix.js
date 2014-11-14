e82447737d7f6816b94c7f1750f4e04d09ed7a04
rogue-syntax <admin@rogue-syntax.com>
2014-11-13 20:05:15 -0500

    fixed some bugs

--- kbdInputOlapfix.js
+++ kbdInputOlapfix.js
@@ -15,22 +15,34 @@ angular.module('kbdInputOlapFix', [])
         link: function($scope, elm, attr) {
  
         	elm[0].addEventListener('focus', function () {
+        		
             	var scrollWin = attr.scrollableWindow;
-            	var scrollderWindow = document.querySelector(".scrollWin");
+            	var scrollderWindow = document.querySelector("."+scrollWin);
             	var scrollderWindowChild = scrollderWindow.children[0];
             	var vHeight = scrollderWindow.offsetHeight;
-            	var elem = elm;
-        	    var height = elem[0].offsetTop;
+            	var elem = elm[0];
+        	    var height = elm[0].offsetTop;
+        	    while (elem = elem.offsetParent) {
+        	    	if ( elem !== scrollderWindow )
+        	    		{
+        				height += elem.offsetTop;
+        				 console.log(elem);
+        	    		}
+        		}
+        	    console.log(height);
         	    var scroller = scrollderWindow.scrollTop;
         	    var relD = height - scroller;
+        	    var marginH = .5*vHeight;
+        	    var placer = height - (marginH*.50);
+        	    /**
+        	    *these are for making an optional smooth scroll in the scrolling service
         	    var dB = vHeight - relD;
         	    var startY = height - (vHeight - dB);
-        	    var marginH = .5*vHeight;
         	    var place = scroller + relD;
         	    var sH = scrollderWindow.scrollHeight;
-        	    var placer = height - (marginH*.50);
+        	    */
         	    var threshold = marginH*.7;
-        	    var hBuffer = marginH+100;
+        	    var hBuffer = marginH;
         	    if (relD > threshold ){
         	    	scrollderWindowChild.setAttribute("style", "padding-bottom:"+hBuffer+"px")
         	    	$scope.$apply();
@@ -47,4 +59,3 @@ angular.module('kbdInputOlapFix', [])
         }
     };
 });
-
